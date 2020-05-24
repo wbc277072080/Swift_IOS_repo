f2fa3b273d17298cad699cc107c78d8706ce6337
wbc277072080 <wang.boch@husky.neu.edu>
2020-05-24 16:19:43 -0400

    upload rich

---
+++ rich_app/rich_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rich_app
+//
+//  Created by 王柏超 on 5/24/20.
+//  Copyright © 2020 Bochao Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
